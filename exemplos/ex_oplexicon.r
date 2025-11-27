80db3204bf35a0d4fddaf4c3d9a4f15421afe485
Christian Martins <martinschristiandavid@gmail.com>
2025-11-26 21:29:24 -0300

    Update ex_oplexicon.r

--- exemplos/ex_oplexicon.r
+++ exemplos/ex_oplexicon.r
@@ -10,7 +10,7 @@ data("oplexicon_v3.0")
 oplexicon <- oplexicon_v3.0
 
 # Prepare o texto
-texto <- "Atentei para todas as obras que se fazem debaixo do sol, e eis que tudo era vaidade e aflição de espírito. Aquilo que é torto não se pode endireitar; aquilo que falta não se pode calcular. Falei eu com o meu coração, dizendo: Eis que eu me engrandeci, e sobrepujei em sabedoria a todos os que houve antes de mim em Jerusalém; e o meu coração contemplou abundantemente a sabedoria e o conhecimento. E apliquei o meu coração a conhecer a sabedoria e a conhecer os desvarios e as loucuras, e vim a saber que também isto era aflição de espírito. Porque na muita sabedoria há muito enfado; e o que aumenta em conhecimento, aumenta em dor."
+texto <- "Atentei para todas as obras que se fazem debaixo do sol, e eis que tudo era vaidade e aflição de espírito. Aquilo que é torto não se pode endireitar; aquilo que falta não se pode calcular. Falei eu com o meu coração, dizendo: Eis que eu me engrandeci, e sobrepujei em sabedoria a todos os que houve antes de mim em Jerusalém; e o meu coração contemplou abundantemente a sabedoria e o conhecimento. E apliquei o meu coração a conhecer a sabedoria e a conhecer os desvarios e as loucuras, e vim a saber que também isto era aflição de espírito. Porque na muita sabedoria há muito enfado; e o que aumenta em conhecimento, aumenta em dor." # eclesiastes 1:14-18   
 df_texto <- tibble(
   id = 1,
   texto = texto
